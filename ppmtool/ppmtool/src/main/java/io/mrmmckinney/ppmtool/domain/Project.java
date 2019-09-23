250c3d3eacc58f355d029f74967638203def0a63
Michael McKinney <sdbuilt.mckinney@gmail.com>
2019-09-23 07:40:27 -0700

    Add some initial validation

--- ppmtool/ppmtool/src/main/java/io/mrmmckinney/ppmtool/domain/Project.java
+++ ppmtool/ppmtool/src/main/java/io/mrmmckinney/ppmtool/domain/Project.java
@@ -2,12 +2,17 @@ package io.mrmmckinney.ppmtool.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Project {
@@ -15,13 +20,26 @@ public class Project {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+  @NotBlank(message = "Project name is required")
   private String projectName;
+  
+  //why have the user come up with a project identifier?
+  @NotBlank(message = "Project identifier is required")
+  @Size(min = 4, max=5, message = "Please use 4-5 characters")
+  @Column(updatable = false, unique =true)
   private String projectIdentifier;
+  
+  @NotBlank(message = "Project description is required")
   private String description;
+  
+  @JsonFormat(pattern = "yyyy-mm-dd")
   private Date start_date;
+  @JsonFormat(pattern = "yyyy-mm-dd")
   private Date end_date;
   
+  @JsonFormat(pattern = "yyyy-mm-dd")
   private Date created_At;
+  @JsonFormat(pattern = "yyyy-mm-dd")
   private Date updated_At;
   
   public Project() {
