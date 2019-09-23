dff732aaac0168642beb0a88f7c9535be8d9da15
mmckinne <michael.mckinney@scientificdrilling.com>
2019-09-23 16:55:39 -0700

    More validation and refactorization

--- ppmtool/ppmtool/src/main/java/io/mrmmckinney/ppmtool/web/ProjectController.java
+++ ppmtool/ppmtool/src/main/java/io/mrmmckinney/ppmtool/web/ProjectController.java
@@ -1,14 +1,19 @@
 package io.mrmmckinney.ppmtool.web;
 
 
+
+
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.mrmmckinney.ppmtool.services.MapValidationErrorService;
 import io.mrmmckinney.ppmtool.services.ProjectService;
 import io.mrmmckinney.ppmtool.domain.Project;
 
@@ -22,14 +27,22 @@ public class ProjectController {
   @Autowired
   private ProjectService projectService;
   
+  @Autowired
+  private MapValidationErrorService mapValidationErrorService;
+  
   //route to actually create a project
   @PostMapping("")
   // use responseEntity to have more control over JSON responses 
-  // for React front end
-  public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+  public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+    
+    ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+    if (errorMap!= null) {
+      return errorMap;
+    }
+    
     //call project service to persist entered project
-    projectService.saveOrUpdateProject(project);
-    return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    Project project1 = projectService.saveOrUpdateProject(project);
+    return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     
   }
   
