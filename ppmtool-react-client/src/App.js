4c97cb61eab1232bee5dcebb07e54c25d7bf8412
mmckinne <michael.mckinney@scientificdrilling.com>
2019-09-25 17:02:11 -0700

    added first react components

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,24 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Michael, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Dashboard />
     </div>
   );
 }
---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import ProjectItem from "./project/ProjectItem";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the Dashboard</h1>
+        <ProjectItem />
+        <ProjectItem />
+        <ProjectItem />
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
